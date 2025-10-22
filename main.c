1bb22d32f612dee7b4028c75fe51dfe8b0f6ed5e
nicolovinci <12999303@itisrossi.vi.it>
2025-10-22 11:02:14 +0200

    Created Struct

--- main.c
+++ main.c
@@ -1,4 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+    char titolo[100];
+    char artista[100];
+    int minuti;
+    short secondi;
+} Canzone;
 
 int main(void) {
     printf("Hello, World!\n");
